6874ec218b40ecb52aca9457fd98f632828d3efc
vijaykanth <87868293+vijaykanth143@users.noreply.github.com>
2024-03-25 18:37:06 +0530

    newsAnytime Demo project

---
+++ src/app/newslist/[articleId]/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ArticleDetails = ({ params }) => {
+  console.log(params, "params");
+  return <div>ArticleDetails</div>;
+};
+
+export default ArticleDetails;
---
+++ src/app/newslist/page.js
@@ -0,0 +1,14 @@
+const NewsList = () => {
+  return (
+    <div>
+      <h1>News List</h1>
+      <ul style={{ listStyle: "none" }}>
+        <li style={{ cursor: "pointer" }}>News 1</li>
+        <li style={{ cursor: "pointer" }}>News 2</li>
+        <li style={{ cursor: "pointer" }}>News 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NewsList;
